cfb44cfd7aa191e50940ca635a967cd9e3b6b24b
Brett Percy <A7GT0ZZ@mmm.com>
2022-12-07 21:23:11 -0600

    adding parent module and customer maven submodule. submod is a web service that connects to postgresql db that runs in docker.

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package com.amigoscode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        //todo check email, firstname, lastname is valid
+
+        customerRepository.save(customer);
+    }
+}
